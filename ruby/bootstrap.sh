ccec308241455904be594fdab03b3d09d6e922ed
Hossam Saraya <hossam.saraya@gmail.com>
2016-05-08 13:02:36 +0200

    remove rv

--- ruby/bootstrap.sh
+++ ruby/bootstrap.sh
@@ -2,6 +2,8 @@
 cd /root/sdk-integration-tests/ruby
 /bin/bash -l -c "rvm use 2.1.8"
 echo -e "[+] Running \033[01;34mbundle install\033[01;30m..."
-/bin/bash -l -c  bundle   > /dev/null 2>&1 #suppress
+/bin/bash -l -c  bundle 
+
+# /bin/bash -l -c  bundle   > /dev/null 2>&1 #suppress
 echo "[+] Running ruby Test Script"
 /bin/bash -l -c "ruby run-test.rb"
