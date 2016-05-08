91c0feb81cef50946dc9f746093825062280f47e
Hossam Saraya <hossam.saraya@gmail.com>
2016-05-08 13:04:12 +0200

    remove rv

--- java/bootstrap.sh
+++ java/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /root/sdk-integration-tests/java
 echo -e "[+] Running \033[01;34mPulling master...\033[01;30m..."
+mkdir cd /root/sdk-integration-tests/java/deps
 cd /root/sdk-integration-tests/java/deps/ && git clone https://github.com/unifiedinbox/ue-java-sdk  >  /dev/null 2>&1
 cd /root/sdk-integration-tests/java/deps/ue-java-sdk && git pull origin master
 echo "[+] Running Java Test Script"
