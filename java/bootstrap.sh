179701fe141efe1fbf82c40d2072ece1c2691a9a
Hossam Saraya <hossam.saraya@gmail.com>
2016-05-14 14:32:07 +0200

    Fix

--- java/bootstrap.sh
+++ java/bootstrap.sh
@@ -2,7 +2,7 @@
 cd /root/sdk-integration-tests/java
 echo -e "[+] Running \033[01;34mPulling master...\033[01;30m..."
 mkdir cd /root/sdk-integration-tests/java/deps
-cd /root/sdk-integration-tests/java/deps/ && git clone https://github.com/unifiedinbox/ue-java-sdk  >  /dev/null 2>&1
+cd /root/sdk-integration-tests/java/deps/ && git clone https://github.com/unifiedinbox/ue-java-sdk
 cd /root/sdk-integration-tests/java/deps/ue-java-sdk && git pull origin master
 echo "[+] Running Java Test Script"
-/bin/bash -l -c "cd /root/sdk-integration-tests/java/src && javac -cp ../deps/ue-java-sdk/dist/ue_java_sdk_jar/ue-java-sdk.jar:. RunTest.java && java -cp ../deps/ue-java-sdk/dist/ue_java_sdk_jar/ue-java-sdk.jar:. RunTest"
+/bin/bash -l -c "cd /root/sdk-integration-tests/java/src && javac -cp /root/sdk-integration-tests/java/deps/ue-java-sdk/dist/ue-java-sdk.jar:. RunTest.java && java -cp /root/sdk-integration-tests/java/deps/ue-java-sdk/dist/ue-java-sdk.jar:. RunTest"
